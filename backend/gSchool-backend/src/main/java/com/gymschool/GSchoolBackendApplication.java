22b174fadd81c24c1c484d705e9d8863971b9456
Fedor <fedorvouynkov@mail.ru>
2025-11-17 15:28:50 +0300

    init skeleton of program

---
+++ backend/gSchool-backend/src/main/java/com/gymschool/GSchoolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gymschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GSchoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GSchoolBackendApplication.class, args);
+	}
+
+}
